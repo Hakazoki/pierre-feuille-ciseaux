9d9c85d03409d20a6168988969c18e5325f2d6d2
Hakazoki <hakozaki@protonmail.ch>
2025-11-17 09:58:58 +0100

    lien css

--- app/index.php
+++ app/index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Shifumi</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <style type="text/tailwindcss"> @theme {        --color-clifford: #da373d;      }    </style>
 </head>
 
 <body>
